31896aaf847b810e8a61ca0d82a964c14758468a
ПК <@DESKTOP-H9D4HFD>
2024-10-23 19:22:35 +0700

    Добавьте файлы проекта.

---
+++ module_7_2.py
@@ -0,0 +1,29 @@
+﻿import io
+from pprint import pprint
+
+def custom_write(file_name, strings):
+    strings_positions = {}
+    file =open(file_name, 'w', encoding='utf-8')
+    pos_ = []
+    text_= []
+    namber_str = 0
+    for i in strings:
+        namber_str +=1
+        pos_.append((namber_str, file.tell()))
+        text_.append(i)
+        file.write(f'{i}\n')
+    file.close()
+    strings_positions = dict(zip(pos_, text_))
+    return strings_positions
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
+
